486e3b06b8c0d121689f20d42259d4b0521ff0c1
devanand <anand.dev.1985@gmail.com>
2016-03-19 22:27:50 +0530

    Added new algorithms

--- src/simple/trees/IsBalancedTree.java
+++ src/simple/trees/IsBalancedTree.java
@@ -1,21 +1,5 @@
 package simple.trees;
 
-class Node {
-
-	public Node left;
-	public int data;
-	public Node right;
-
-	public Node(int data) {
-		this.data = data;
-	}
-
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return this.data+"";
-	}
-}
 
 public class IsBalancedTree {
 
